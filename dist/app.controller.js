01f59da9ee3e7744c8595eada7c882c721237e18
Changhui <cckdgml1031@gmail.com>
2024-08-07 19:29:13 +0900

    test build

--- dist/app.controller.js
+++ dist/app.controller.js
@@ -8,6 +8,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppController = void 0;
 const common_1 = require("@nestjs/common");
@@ -16,11 +19,21 @@ let AppController = class AppController {
     constructor(appService) {
         this.appService = appService;
     }
+    getIndex(res) {
+        res.sendFile('index.html', { root: 'dist' });
+    }
     getHello() {
         return this.appService.getHello();
     }
 };
 exports.AppController = AppController;
+__decorate([
+    (0, common_1.Get)('*'),
+    __param(0, (0, common_1.Res)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], AppController.prototype, "getIndex", null);
 __decorate([
     (0, common_1.Get)('/hello'),
     __metadata("design:type", Function),
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+
+  @Get('*')
+  getIndex(@Res() res: Response) {
+    res.sendFile('index.html', { root: 'dist' }); // dist 폴더의 index.html 파일을 반환
+  }
 
   @Get('/hello')
   getHello(): string {
